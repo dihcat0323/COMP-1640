f308292566c94a8681118ea0eb45fee61bf1a118
Reginald Gibson <rsprometheus@gmail.com>
2018-03-02 23:21:17 +0700

    Revert "Revert "Model classes for Category and Comment based on current db.""
    This reverts commit af6e86da895a8d34033b31d468c1f9f9fbb7b84e.

---
+++ COMP - 1640/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP___1640.Models
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+        public String categoryName { get; set; }
+        public String categoryDesc { get; set; }
+    }
+}
