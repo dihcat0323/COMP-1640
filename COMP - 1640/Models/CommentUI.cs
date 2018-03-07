3e04e870681ec398b1f729440c7e132cefe32ac9
minhd <minhdngt00554@fpt.edu.vn>
2018-03-07 19:33:58 +0700

    add missing class CommentUI

--- COMP - 1640/Models/CommentUI.cs
+++ COMP - 1640/Models/CommentUI.cs
@@ -7,13 +7,8 @@ namespace COMP___1640.Models
 {
     public class CommentUI
     {
-        //public int Id { get; set; }
-        //public int ideaId { get; set; }
         public string userName { get; set; }
         public string Details { get; set; }
-        //public bool isAnonymous { get; set; }
         public string postedDate { get; set; }
-
-
     }
 }
